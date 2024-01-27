83296929d5046826bc3073d9159bf9f86124ef51
jksandy <santhoshsingh2002@gmail.com>
2024-01-27 22:42:27 +0530

    Section 1 Code completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio-0';
+  nav_items = [
+    {
+      button_content: "Education",
+      span_content: "school"
+    },
+    {
+      button_content: "Technical Skills",
+      span_content: "code"
+    },
+    {
+      button_content: "Acheivements",
+      span_content: "trophy"
+    },
+    {
+      button_content: "Experience",
+      span_content: "work"
+    },
+    {
+      button_content: "Projects",
+      span_content: "wifi"
+    }
+  ]
 }
